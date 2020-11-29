808a125dabe219b4565a43c0c852bedbfeb903e2
raurodmen1997 <raulrodriguezmendez1997@gmail.com>
2020-11-29 18:33:15 +0100

    Proyecto base backend

---
+++ src/main/java/com/cosateca/apirest/CosatecaApirestApplication.java
@@ -0,0 +1,13 @@
+package com.cosateca.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosatecaApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosatecaApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosateca/apirest/CosatecaApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosateca.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosatecaApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
